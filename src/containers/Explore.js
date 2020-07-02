47f7ca21a29e44b83bc12276f795167a5392d0f6
stukene <estuken53@gmail.com>
2020-07-02 13:41:49 -0700

    create skeleton for explore page

---
+++ src/containers/Explore.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { api } from "../services/api";
+
+class Explore extends Component {
+
+
+    state = {
+
+    }
+
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Explore;
