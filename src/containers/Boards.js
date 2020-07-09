64d32964fe63cc0bed38bb1dbf2cd08ecb75f50e
stukene <estuken53@gmail.com>
2020-07-09 15:31:14 -0700

    Added Boards container

---
+++ src/containers/Boards.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { api } from "../services/api";
+
+class Boards extends Component {
+
+
+    state = {
+
+    }
+
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Boards;
