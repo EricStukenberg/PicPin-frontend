1dbb18a93a44271024b59540467235c1857a99a7
stukene <estuken53@gmail.com>
2020-07-09 15:36:30 -0700

    Added Profile

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Explore from './containers/Explore';
+import Boards from './containers/Boards';
+import Profile from './containers/Profile';
+
+
 
 function App() {
   return (
@@ -11,6 +14,10 @@ function App() {
         <Switch>
           <Route exact path='/explore' render={props => 
             <Explore {...props}/>}/>
+          <Route exact path='/boards' render={props => 
+            <Boards {...props}/>}/>
+          <Route exact path='/profile' render={props => 
+            <Profile {...props}/>}/>
         </Switch>
       </Router>
 
