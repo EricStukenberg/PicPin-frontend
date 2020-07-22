e82caeb42181eb87ffadb290e6ade8dd60773fe2
stukene <estuken53@gmail.com>
2020-07-22 15:16:29 -0700

    nav changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Explore from './containers/Explore';
 import Boards from './containers/Boards';
 import Profile from './containers/Profile';
+import About from './containers/About';
+import NavBar from './components/NavBar.js';
 
 
 
-function App() {
-  redner () {
+
+
+class App extends Component {
+  render () {
     return (
-      <div className="App">
+      <div>
         <Router >
+          <NavBar />
           <Switch>
             <Route exact path='/explore' render={props => 
               <Explore {...props}/>}/>
@@ -19,6 +24,8 @@ function App() {
               <Boards {...props}/>}/>
             <Route exact path='/profile' render={props => 
               <Profile {...props}/>}/>
+            <Route exact path='/about' render={props => 
+              <About {...props}/>}/>
           </Switch>
         </Router>
 
