a8ac05d14060234d4ed51817eb300d1eb983954e
stukene <estuken53@gmail.com>
2020-07-23 14:47:48 -0700

    imageCard

---
+++ src/fragments/ImageCard.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from 'react';
+
+
+const ImageCard = props => {
+
+    return(
+        <Fragment>
+            <section id="photos">
+
+            </section>
+        </Fragment>
+    )
+
+    
+
+
+}
+
+export default ImageCard;
