92fce974dd6c5cb5c9e9f01533bcc6878c47d6b7
stukene <estuken53@gmail.com>
2020-07-23 16:13:27 -0700

    explore changes

--- src/containers/Explore.js
+++ src/containers/Explore.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
 import { api } from "../services/api";
-
+// for testing 
+const API = 'http://localhost:3000/search_terms/random';
 class Explore extends Component {
 
 
     state = {
-
+        pics: []
     }
 
 
     componentDidMount() {
+        fetch(API)
+        .then(res => res.json())
+        .then(
+          (result) => {
+            console.log(result)
+            this.setState({
+                pics: result
+            });
+
+          },
+  
+          (error) => {
+            console.log(error)
+          }
+        )
 
     }
 
