1f12f5b16c328fd4b10766176113d4cacc873267
stukene <estuken53@gmail.com>
2020-07-29 14:24:03 -0700

    Created Profile card

---
+++ src/fragments/ProfileCard.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from 'react';
+
+
+const ProfileCard = props => {
+
+    return(
+        <Fragment>
+            <section id="photos">
+
+            </section>
+        </Fragment>
+    )
+
+    
+
+
+}
+
+export default ProfileCard;
